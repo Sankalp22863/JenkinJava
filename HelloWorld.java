0d0f3ce9e7e9c4c7fef97c13dc12421b60fccf92
Sankalp Naik <51851043+Sankalp22863@users.noreply.github.com>
2022-08-24 14:47:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		System.out.println("How you doing??");
+		System.out.println("Joey doesn't share food.");
+		System.out.println("Monica Chill!!! Go get a beer.");
+		System.out.println("Who said it was for you??");
 
 	}
 
